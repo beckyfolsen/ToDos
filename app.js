35ac2ecb82790a032d711dd4dbcbfa10f49f216e
beckyfolsen <bckolsn@gmail.com>
2019-04-18 17:07:16 -0400

    fixed checkbox styling

--- app.js
+++ app.js
@@ -1,54 +1,71 @@
-function onReady() {
-  const addToDoForm = document.getElementById("addToDoForm");
-  const newToDoText = document.getElementById("newToDoText");
-  const toDoList = document.getElementById("toDoList");
+function onReady(){
+    const addToDoForm = document.getElementById('addToDoForm');
+    const newToDoText = document.getElementById('newToDoText');
+    const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit', event => {
-      event.preventDefault();
+    addToDoForm.addEventListener('submit', () => {
+        event.preventDefault();
+        
+        //create list item
+        let newLi = document.createElement('li');  
+        newLi.classList.add('mdl-list__item', 'newMdl');
 
-      // get the text
-      let title = newToDoText.value;
+        //create primary span container
+        let toDoContainer = document.createElement('span');
+        toDoContainer.classList.add('mdl-list__item-primary-content', 'newMdl');
 
-      // create a new li
-      let newLi = document.createElement('li');
+        //create checkbox and attach to primary span container
+        let toDoLabel = document.createElement('label');
+        toDoLabel.classList.add('mdl-checkbox', 'mdl-js-checkbox', 'mdl-js-ripple-effect', 'newMdl');
+        toDoLabel.htmlFor = 'list-checkbox-1';
+        let checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = 'list-checkbox-1';
+        checkbox.classList.add('mdl-checkbox__input', 'newMdl');
 
-      // create a new input
-      let checkbox = document.createElement('input');
 
-      // set the input's type to checkbox
-      checkbox.type = "checkbox";
-      
-      checkbox.id = "check";
-      checkbox.className = "mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect";
-      
+        //create text and attach to primary span container
+        let labelText = document.createTextNode(newToDoText.value);
+        
 
-      // create a delete button
-      let deleteBtn = document.createElement('button');
-      deleteBtn.className = 'delete mdl-button mdl-js-button mdl-button--raised mdl-button--colored';
+        //append primary span container container to list item
+        
 
-      deleteBtn.textContent = "Delete";
+        //create secondary span container
+        let deleteContainer = document.createElement('span');
+        deleteContainer.classList.add('mdl-list__item-secondary-action', 'newMdl');
 
-      deleteBtn.addEventListener('click', function(event){
-          toDoList.removeChild(this.parentElement);
-      })
+        //create delete button and append to secondary span container
+        let deleteButton = document.createElement('button');
+        deleteButton.classList.add('mdl-button', 'mdl-js-button', 'mdl-button--icon', 'newMdl');
+        let icon = document.createElement('i');
+        let text = document.createTextNode('delete');
+        icon.classList.add('material-icons');
+        icon.appendChild(text);
+        deleteButton.appendChild(icon);
+        
+        deleteContainer.addEventListener('click', function(e) {
+            toDoList.removeChild(deleteButton.parentNode.parentNode);
+        });
+        
+        deleteContainer.appendChild(deleteButton);
+        toDoLabel.appendChild(checkbox);
+        newLi.appendChild(toDoContainer);
+        toDoContainer.appendChild(toDoLabel);
+        toDoContainer.appendChild(labelText);
+        //append secondary span container to list item
+        newLi.appendChild(deleteContainer);
 
-      // set the title
-      newLi.textContent = title;
+        //add list item to to-do list
+        toDoList.appendChild(newLi);
 
-      // attach the checkbox to the li
-      newLi.appendChild(checkbox);
-
-      // attach the delete button to the li
-      newLi.appendChild(deleteBtn);
-
-      // attach the li to the ul
-      toDoList.appendChild(newLi);
-
-      //empty the input
-      newToDoText.value = '';
-  });
+        //empty form input
+        newToDoText.value = '';
+        let mdlElemets = document.querySelectorAll(".newMdl");
+        componentHandler.upgradeElements(mdlElemets);
+    });
 }
 
 window.onload = function() {
-  onReady();
- };
+   onReady();
+}
